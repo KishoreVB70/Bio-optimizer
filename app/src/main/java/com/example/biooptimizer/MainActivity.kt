10078e039763dac33c72bd81f462e45ffb67ed3e
kishore <kishore07kzr@gmail.com>
2024-04-06 17:49:13 +0530

    changed app colour, added app icon and added inside icon also

--- app/src/main/java/com/example/biooptimizer/MainActivity.kt
+++ app/src/main/java/com/example/biooptimizer/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         // Python
         if (! Python.isStarted()) {
             Python.start(AndroidPlatform(this));
